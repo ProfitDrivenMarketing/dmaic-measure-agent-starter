ed462dde48bf411917c222578e8ce3c30d54fb76
ProfitDrivenMarketing <admin@profitdrivenmarketing.com>
2025-08-13 09:13:29 +0100

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,5 @@
+# Make "app.schemas" importable as an alias of "app.models.schemas"
+import sys
+from .models import schemas as _schemas_module
+sys.modules[__name__ + ".schemas"] = _schemas_module
 
